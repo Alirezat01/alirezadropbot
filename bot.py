07e09cbed225e6836986b6778affe1613e3770a3
Alirezat01 <tajvidinejad@gmail.com>
2025-02-15 12:43:04 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 import requests
 import os  # برای دریافت توکن از متغیر محیطی
 
-TOKEN = os.getenv("BOT_TOKEN")  # دریافت توکن از Railway
+TOKEN = os.getenv("7689008198:AAFj2rjuRcqSramnQTqJGAp_R8OM0rmKymE")  # دریافت توکن از Railway
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
